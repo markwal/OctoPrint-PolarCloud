e45f3acff2ccda92d12f25d3f79be7c9c4c04a16
Mark Walker <markwal@hotmail.com>
2017-07-03 17:01:46 -0700

    0.2.0 Beta

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 plugin_identifier = "polarcloud"
 plugin_package = "octoprint_polarcloud"
 plugin_name = "OctoPrint-PolarCloud"
-plugin_version = "0.1.0Beta"
+plugin_version = "0.2.0Beta"
 plugin_description = """Connects OctoPrint to the PolarCloud so you can easily monitor and control outside of your local network"""
 plugin_author = "Mark Walker"
 plugin_author_email = "markwal@hotmail.com"
